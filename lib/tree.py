cc7a8409f4c2362481f3ecc25878ddbbbdbeb350
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-03-20 08:13:53 +0300

    Completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,22 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+
+        queue = [self.root]
+        front = 0
+
+        while front < len(queue):
+            node = queue[front]
+            front += 1
+
+            if node.get('id') == id:
+                return node
+
+            for child in node.get('children', []):
+                queue.append(child)
+
+        return None
